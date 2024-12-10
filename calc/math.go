0b9098158b49be56684013117adbfd75e768723a
sachinkumawatcoindcx <sachin.kumawat@coindcx.com>
2024-12-10 09:23:40 +0530

    v2

--- calc/math.go
+++ calc/math.go
@@ -1,9 +1,16 @@
 package calc
 
-func Add(numbers ...int) int { // variadic function
+import "errors"
+
+func Add(numbers ...int) (error, int) { // variadic function
 	result := 0
-	for _, number := range numbers {
-		result += number
+
+	if len(numbers) < 2 {
+		return errors.New("provide more than 1 number"), result
+	} else {
+		for _, number := range numbers {
+			result += number
+		}
+		return nil, result
 	}
-	return result
 }
